67fa582126fe28f39fe88c341437379351c74a86
Tarun Aggarwal <tarun@iotric.com>
2025-10-12 14:36:16 +0530

    stocke market

---
+++ stock-market/app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import TradingViewWidget from '@/components/TradingViewWidget'
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from '@/lib/constants'
+import React from 'react'
+
+function Home() {
+    const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
+    return (
+        <div className='flex min-h-screen home-wrapper'>
+            <div className="grid w-full gap-8 home-section">
+                {/* TradingView Widget */}
+                <div className="md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        title="Market Overview"
+                        scriptUrl={`${scriptUrl}market-overview.js`}
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ stock-market/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import Navitems from './Navitems';
+import UserDropdown from './UserDropdown';
+
+function Header({ user }: { user: User }) {
+    return (
+        <header className='sticky top-0 header'>
+            <div className="container header-wrapper">
+                <Link href="/" className='text-2xl font-bold'>
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                </Link>
+                <nav className='hidden sm:block'>
+                    <Navitems />
+                </nav>
+                <UserDropdown user={user} />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
